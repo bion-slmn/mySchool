b3b6d68d680781a80d500f2c7903d117fc6c8035
bion-slmn <bionsol25@gmail.com>
2024-08-21 18:48:28 +0300

    added login page

--- my_school/settings.py
+++ my_school/settings.py
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
     'school_fee',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -88,7 +90,7 @@ DATABASES = {
     }
 }
 
-
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
